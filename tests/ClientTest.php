4e48ebbe6a6a75a07f6f29370c7c97a4312e98dd
thomas <thomas@sonicmoov.com>
2017-01-04 12:30:25 +0900

    edit teat

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -135,7 +135,7 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         try {
             $simpleApi->send();
         } catch (Exception $e) {
-            $this->addToAssertionCount($e->getMessage(), "Couldn't resolve host '404.error'");
+            $this->assertEquals($e->getMessage(), "Couldn't resolve host '404.error'");
         }
     }
 
