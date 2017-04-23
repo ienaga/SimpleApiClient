be4878ba5a1fb023592b626ea730d8becc1a30a5
thomas <thomas@sonicmoov.com>
2017-04-23 22:59:19 +0900

    edit test case

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,8 +2,8 @@
 
 require_once __DIR__ . "/../src/api/Client.php";
 
-if (class_exists('\PHPUnit\Framework\TestCase') &&
-    !class_exists('\PHPUnit_Framework_TestCase')) {
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
     class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
 }
 
