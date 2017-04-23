ddc0d062b8689de26709c82a27d43cb499e7bc40
thomas <thomas@sonicmoov.com>
2017-04-23 22:40:26 +0900

    edit test case

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,9 +2,12 @@
 
 require_once __DIR__ . "/../src/api/Client.php";
 
-require __DIR__ . "/../../vendor/autoload.php";
+if (!class_exists("\\PHPUnit\\Framework\\TestCase") &&
+    class_exists("\\PHPUnit_Framework_TestCase")) {
+    class_alias("\\PHPUnit_Framework_TestCase", "\\PHPUnit\\Framework\\TestCase");
+}
 
-class ClientTest extends \PHPUnit_Framework_TestCase
+class ClientTest extends PHPUnit_Framework_TestCase
 {
 
     /**
